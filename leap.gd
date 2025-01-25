f902e0d40d99a942e9538aa25f8b1d1c5a53a709
brave-samurai <skovvboy@gmail.com>
2025-01-25 15:40:43 +0000

    bouncing and rat fix

--- leap.gd
+++ leap.gd
@@ -8,4 +8,4 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress_ratio += 0.05
+	progress_ratio += 0.005
