f49d8eacd7a1c9a7b66394797c6dd3c340e95803
brave-samurai <skovvboy@gmail.com>
2025-01-25 18:50:25 +0000

    fjbneju

--- ball.gd
+++ ball.gd
@@ -1,10 +1,3 @@
 extends CharacterBody2D
 
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float):
-	if is_on_floor():
-		$AnimatedSprite2D.play("bounce")
+const UP_DIRECTION := Vector2.UP
--- scenes/ball.gd
+++ scenes/ball.gd
@@ -15,3 +15,6 @@ func _process(delta: float) -> void:
 func _on_area_2d_area_entered(area: Area2D):
 	print("colliding?")
 	$AnimatedSprite2D.play("bounce")
+
+
+#physics get velocity of ball and flip to negative
