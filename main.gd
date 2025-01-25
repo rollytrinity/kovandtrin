1c1483b84746d9a13c32d397832cb3c820893c4a
rollytrinity <gullandtrinity@gmail.com>
2025-01-25 20:07:04 +0000

    commiting

--- main.gd
+++ main.gd
@@ -18,8 +18,7 @@ func _on_body_entered(body: RigidBody2D):
 	print("disabled?")
 	$Wall/tunnelwall/CollisionPolygon2D.disabled = false
 	$Wall/tunnelwall/CollisionPolygon2D.set_deferred("disabled", false)
-<<<<<<< HEAD
-=======
+
 	
 	var tunnel1 = $Wall/tunnelTexture1
 	var tunnel2 = $Wall/tunnelTexture2
@@ -27,7 +26,7 @@ func _on_body_entered(body: RigidBody2D):
 	tunnel2.reparent($Wall/upperground)
 	
 	
->>>>>>> trin
+
 
 func _on_rat_detect_body_entered(body: RigidBody2D):
 	$Wall/Path2D/PathFollow2D.progress_ratio = 0.05
