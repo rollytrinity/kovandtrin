c1b45023f9b0f5179be722098e8d2a973d4c53e5
rollytrinity <gullandtrinity@gmail.com>
2025-01-26 10:59:49 +0000

    fuck my life

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -12,9 +12,7 @@ func _process(delta: float) -> void:
 	pass
 
 
+#staticbody for area instead?
 func _on_area_2d_area_entered(area: Area2D):
 	print("colliding?")
 	$AnimatedSprite2D.play("bounce")
-
-
-#physics get velocity of ball and flip to negative
